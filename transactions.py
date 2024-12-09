02918c710b767478d48414ae1aeb23e470e8f2b3
Yerdos <gayka101@gmail.com>
2024-12-09 14:38:51 +0500

    Merged bobur into erdos

--- transactions.py
+++ transactions.py
@@ -7,18 +7,18 @@ products_file = 'Database/products.json'  # JSON-файл с товарами н
 
 
 # Загрузка существующих данных (транзакции и товары)
-def load_data(path, ):
-    try:
-        with open(file_path, 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return []  # Если файл отсутствует, возвращаем пустой список
-
-
-# Сохранение данных в файл
-def save_data(data, file_path):
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=4)
+# def load_data(path, ):
+#     try:
+#         with open(file_path, 'r') as file:
+#             return json.load(file)
+#     except FileNotFoundError:
+#         return []  # Если файл отсутствует, возвращаем пустой список
+#
+#
+# # Сохранение данных в файл
+# def save_data(data, file_path):
+#     with open(file_path, 'w') as file:
+#         json.dump(data, file, indent=4)
 
 
 # Проверка наличия товара на складе для экспорта и уменьшение количества
