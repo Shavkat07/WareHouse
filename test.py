bef5ff8d2228243df62cca0449e95f74916a2334
Yerdos <gayka101@gmail.com>
2024-12-16 08:58:16 +0500

    prosta commit

---
+++ test.py
@@ -0,0 +1,11 @@
+# from transactions import view_transactions
+# view_transactions()
+from products import view_products
+from transactions import delete_transaction, create_transaction, check_product_availability
+
+# delete_transaction()
+
+# print(view_products())
+
+# create_transaction()
+check_product_availability()
